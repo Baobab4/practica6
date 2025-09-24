0b0528b586e9bf51a38a98936754675500c0380f
315 <315@315-29>
2025-09-24 11:35:24 +0300

    Выполнен расчет значения F

--- practica6/Program.cs
+++ practica6/Program.cs
@@ -13,6 +13,7 @@ namespace practica6
             Console.Write("Введите значения параметра x = ");
             string s = Console.ReadLine();
             double x = Convert.ToDouble(s);
+            double F = 2 * Math.Cos(x);
 
         }
     }
